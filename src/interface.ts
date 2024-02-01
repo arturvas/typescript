3a9a0cd9b302e9ee3bf4d2adc6b44e06c9d94db7
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2024-02-01 17:40:19 -0400

    interface em typescript

---
+++ src/interface.ts
@@ -0,0 +1,27 @@
+class Conta {
+  numeroConta: number;
+  saldo: number = 0;
+
+  constructor(numeroConta: number) {
+    this.numeroConta = numeroConta;
+  }
+}
+
+class ContaSalario extends Conta {
+  depositar(valor: number) {
+    this.saldo += valor;
+  }
+}
+
+interface ITransacional {
+  transferir: (valor: number, destinatario: Conta) => boolean;
+  taxaTransferencia: number;
+}
+
+class ContaCorrente extends Conta implements ITransacional {
+  transferir(valor: number, destinatario: Conta) {
+    destinatario.saldo += valor - this.taxaTransferencia;
+    return true;
+  }
+  taxaTransferencia: number = 0;
+}
